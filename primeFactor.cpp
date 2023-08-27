d4c5da56740a8d468017ff8208321973112561bb
Ajinkya Narkhede <46190988+ajinkyavn1@users.noreply.github.com>
2023-08-27 21:18:18 +0530

    primeFactor using sive

--- primeFactor.cpp
+++ primeFactor.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
-int n=1e7+5;
-vector<bool> isPrime(n,1);
+using namespace std;
+const int  n=1e7+10;
+
 int main(){
-    for(int i=2;i<n;i++){
+    vector<bool> isPrime(n,1);
+    isPrime[0]=isPrime[1]=false;
+    for(int i=2;i<n;++i){
         if(isPrime[i]==true){
-            for(int j=2*i;j<n;j+=2){
+            for(int j=2*i;j<n;j+=i){
                 isPrime[j]=false;
             }
         }
     }
-    for(auto a:isPrime){
-        cout<<a<<"\n";
-    }
+   for(auto i=0;i<1000;i++){
+    cout<<"\n"<<isPrime[i]<<" "<<i;
+   }
     return 0;
 }
